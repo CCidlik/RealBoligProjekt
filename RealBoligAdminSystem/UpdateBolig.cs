ef77db1483078bbddada6710178699790e4b4dc3
kaspermark <kaspermarks@gmail.com>
2021-12-07 13:39:47 +0100

    Update UpdateBolig.cs
    Slettet solgt cariable

--- RealBoligAdminSystem/UpdateBolig.cs
+++ RealBoligAdminSystem/UpdateBolig.cs
@@ -40,7 +40,7 @@ namespace RealBolig
 
             if (CbBoligSolgt.Checked != true)
             {
-                bool Solgt = false;
+                
 
                 // length check:
                 if (Salgspris.Length > 23) Salgspris_ok = false;
