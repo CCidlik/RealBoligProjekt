87cfc0a9e9605bfbf6b1b85a5414408cbae1b633
RasmusHS <rasshoey@gmail.com>
2021-12-10 11:17:52 +0100

    Completed opg. 3.2

--- RealBoligAdminSystem/ReadBolig.cs
+++ RealBoligAdminSystem/ReadBolig.cs
@@ -11,6 +11,7 @@ using System.Data.SqlClient;
 using System.Text.RegularExpressions;
 using RealBolig.DAL.Entities;
 using RealBolig.DAL.Operations;
+using System.IO;
 
 namespace RealBolig
 {
@@ -43,6 +44,7 @@ namespace RealBolig
 
 
         }
+
         public void DisplayDataOmråde()
         {
 
@@ -71,6 +73,203 @@ namespace RealBolig
             adapt.Fill(dt);
             dataGridView1.DataSource = dt;
             conn.Close();
+        }
+
+        private void BtnUdprintHuseTilSalg_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConnString.getConnStr());
+
+            try
+            {
+                conn.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapt = new SqlDataAdapter("SELECT * FROM Bolig WHERE Solgt = 0", conn);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR: \n\n" + exc.ToString());
+
+            }
+
+            string userName = Environment.UserName;
+            string path = $@"C:\Users\{userName}\Documents\BoligData";
+
+            try
+            {
+                if(Directory.Exists(path))
+                {
+                    using (TextWriter tw = new StreamWriter(path + "\\AlleHuseTilSalg.txt"))
+                    {
+                        for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                        {
+                            for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                            {
+                                tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+
+                                if (j != dataGridView1.Columns.Count - 1)
+                                {
+                                    tw.Write("  :  ");
+                                }
+                            }
+                            tw.WriteLine();
+                        }
+                    }
+                }
+
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
+
+            catch(Exception exc)
+            {
+                MessageBox.Show("ERROR: \n\n" + exc.ToString());
+            }
+
+            using (TextWriter tw = new StreamWriter(path+"\\AlleHuseTilSalg.txt"))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+
+                        if (j != dataGridView1.Columns.Count - 1)
+                        {
+                            tw.Write("  :  ");
+                        }
+                    }
+                    tw.WriteLine();
+                }
+            }
+        }
+
+        private void BtnUdskrivData_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConnString.getConnStr());
+
+            try
+            {
+                conn.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapt = new SqlDataAdapter("SELECT * FROM Bolig WHERE Område LIKE '%" + tbOmråde.Text + "%'", conn);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR: \n\n" + exc.ToString());
+
+            }
+
+            string userName = Environment.UserName;
+            string path = $@"C:\Users\{userName}\Documents\BoligData";
+
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    using (TextWriter tw = new StreamWriter(path + "\\AlleHuseIOmråde.txt"))
+                    {
+                        for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                        {
+                            for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                            {
+                                tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+
+                                if (j != dataGridView1.Columns.Count - 1)
+                                {
+                                    tw.Write("  :  ");
+                                }
+                            }
+                            tw.WriteLine();
+                        }
+                    }
+                }
+
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
+
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR: \n\n" + exc.ToString());
+            }
+
+            using (TextWriter tw = new StreamWriter(path + "\\AlleHuseIOmråde.txt"))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+
+                        if (j != dataGridView1.Columns.Count - 1)
+                        {
+                            tw.Write("  :  ");
+                        }
+                    }
+                    tw.WriteLine();
+                }
+            }
+        }
+
+        private void tbOmråde_TextChanged(object sender, EventArgs e)
+        {
+            if (tbOmråde.Text != "")
+            {
+                DisplayOmråde();
+            }
+
+            else
+            {
+                RefreshDGV();
+            }
+        }
+
+        public void DisplayOmråde()
+        {
+            SqlConnection conn = new SqlConnection(ConnString.getConnStr());
+
+            try
+            {
+                conn.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapt = new SqlDataAdapter("SELECT * FROM Bolig WHERE Område LIKE '%" + tbOmråde.Text + "%'", conn);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR: \n\n" + exc.ToString());
+                tbOmråde.Text = "";
+            }
+        }
+
+        public void RefreshDGV()
+        {
+            SqlConnection conn = new SqlConnection(ConnString.getConnStr());
+
+            try
+            {
+                conn.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapt = new SqlDataAdapter("SELECT * FROM Bolig", conn);
+                adapt.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR: \n\n" + exc.ToString());
+                tbOmråde.Text = "";
+            }
         }
     }
 }
